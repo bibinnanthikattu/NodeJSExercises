37dcc9bb10cd04e88d481d904d8b0d846ebbfe38
Bibin Nandikattu Jose <bibinnanthikattu@hotmail.com>
2022-08-02 10:26:28 +0200

    Feat:REPL random ID generator

---
+++ REPL/script.mjs
@@ -0,0 +1,11 @@
+import Crypto from 'crypto';
+
+const Id = Crypto.randomUUID();
+console.log(Id);
+
+// In terminal too we can use REPL
+
+// node : enter
+// global. :double tab
+// global.crypto : lists the functions inside this crypto module
+// cyrpto.radnumUUID() : generates a random Id
